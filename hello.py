d8133b6822d0b07e294fb0e134c0e02565411362
manh <manhld1512003@gmail.com>
2026-08-24 22:23:35 +0700

    hello

---
+++ hello.py
@@ -0,0 +1,284 @@
+ERROR = {
+    "signup_exist": {
+        "en": "Username already exists, please try again",
+        "vi": "Tài khoản này đã tồn tại, xin vui lòng thử lại"
+    },
+    "signup_blank": {
+        "en": "Username cannot be blank, please try again",
+        "vi": "Tên tài khoản không được để trống, xin vui lòng thử lại"
+    },
+    "signup_failed": {
+        "en": "Account registration failed",
+        "vi": "Đăng kí tài khoản không thành công"
+    },
+    "account_lock": {
+        "en": "Account has been locked",
+        "vi": "Tài khoản đã bị khóa"
+    },
+    "account_wrong": {
+        "en": "Incorrect account or password information",
+        "vi": "Thông tin tài khoản hoặc mật khẩu không đúng"
+    },
+    "login_faild": {
+        "en": "An error occurred in the system",
+        "vi": "Đã xảy ra lỗi trong quá trình đăng nhập"
+    },
+    "logout_msg": {
+        "en": "You have successfully logged out",
+        "vi": "Bạn đã đang xuất thành công"
+    },
+    "dataset_notexist": {
+        "en": "Dataset does not exist",
+        "vi": "Bộ dữ liệu không tồn tại"
+    },
+    "category_error": {
+        "en": "Error getting list of labels",
+        "vi": "Lỗi xảy ra khi lấy danh sách"
+    },
+    "dataset_nolabel": {
+        "en": "No images without labels", 
+        "vi": "Không có ảnh không có nhãn"
+    },
+    "category_update": {
+        "en": "New label has no change compared to the old label",
+        "vi": "Nhãn mới không có sự thay đổi so với nhãn cũ"
+    },
+    "category_notfound": {
+        "en": "Label is not in the dataset",
+        "vi": "Nhãn không có trong tập dữ liệu"
+    },
+    "camera_error": {
+        "en": "An error has occurred",
+        "vi": "Đã có lỗi xảy ra"
+    },
+    "camera_not_exist": {
+        "en": "Camera does not exist",
+        "vi": "Camera không tồn tại"
+    },
+    "camera_name_exist": {
+        "en": "Camera name already exist",
+        "vi": "Tên camera đã tồn tại"
+    },
+    "camera_not_working": {
+        "en": "Camera does not working",
+        "vi": "Camera không hoạt động"
+    },
+    "camera_not_actived": {
+        "en": "Camera not activated",
+        "vi": "Camera chưa được kích hoạt"
+    },    
+    "stream_failed": {
+        "en": "Failed to start stream reader thread",
+        "vi": "Không thể mở stream, vui lòng kiểm tra đường dẫn stream"
+    }, 
+    "stream_url_error": {
+        "en": "Could not open stream, check URL status",
+        "vi": "Không thể mở stream, vui lòng kiểm tra đường dẫn stream"
+    },  
+
+    "user_notfound": {
+        "en": "User ID does not exist",
+        "vi": "ID người dùng không tồn tại"
+    },
+    "dataset_strip": {
+        "en": "The dataset name cannot be empty",
+        "vi": "Tên bộ dữ liệu không được để trống"
+    },
+    "dataset_exists": {
+        "en": "Dataset name already exists, please try again",
+        "vi": "Tên bộ dữ liệu này đã tồn tại"
+    },
+    "label_space": {
+        "en": "There is an empty label in the list",
+        "vi": "Có nhãn trống trong danh sách"
+    },
+    "dataset_failed": {
+        "en": "New dataset creation failed",
+        "vi": "Tạo mới bộ dữ liệu không thành công"
+    },
+    "dataset_zip": {
+        "en": "The file is not a zip file",
+        "vi": "Tệp được gửi không phải là têp zip"
+    },
+    "dataset_zip_not_exist": {
+        "en": "The file is not exist",
+        "vi": "Tệp được gửi không tồn tại"
+    },
+    "path_file": {
+        "en": "Unable to add files, please try again",
+        "vi": "Không thể thêm file vui lòng thử lại"
+    },
+    "upload_file": {
+        "en": "Error file is missing during upload",
+        "vi": "Lỗi file bị thiếu trong quá trình tải lên"
+    },
+    "not_memory": {
+        "en": "Not enough memory",
+        "vi": "Không đủ bộ nhớ"
+    },
+    "not_image": {
+        "en": "Image not found in dataset",
+        "vi": "Không tìm thấy ảnh trong bộ dữ liệu"
+    },
+    "label_strip": {
+        "en": "Label is empty, please try again", 
+        "vi": "Nhãn đang bị trống vui lòng nhập lại"
+    },
+    "label_image_exists": {
+        "en": "The image contains a label, the label cannot be removed",
+        "vi": "Còn ảnh chứa nhãn không thể xóa"
+    },
+    "label_exists": {
+        "en": "Label already exists",
+        "vi": "Nhãn đã tồn tại"
+    },    
+    "permission": {
+        "en": "You do not have permission to perform this operation",
+        "vi": "Bạn không có quyền thực hiện thao tác này"
+    },
+    "dataset_validation": {
+        "en": "Dataset validation error",
+        "vi": "Lỗi xác nhận bộ dữ liệu"
+    },
+    "dataset_error": {
+        "en": "An error has occurred",
+        "vi": "Đã có lỗi xảy ra"
+    },
+    "dataset_confirm": {
+        "en": "Dataset is awaiting confirmation",
+        "vi": "Bộ dữ liệu đang chờ xác nhận"
+    },
+    "dataset_not_model": {
+        "en": "No predictive model found",
+        "vi": "Không tìm thấy mô hình dự đoán"
+    },
+    "download_error": {
+        "en": "Download file error",
+        "vi": "Tải xuống file không thành công"
+    },    
+    "not_pathfile": {
+        "en": "The folder to save data could not be found",
+        "vi": "Không tìm thấy thư mục lưu dữ liệu"
+    },
+    "model_exist": {
+        "en": "Model does not exist",
+        "vi": "Mô hình không tồn tại"
+    },
+    "model_deploy": {
+        "en": "The model has not been implemented yet",
+        "vi": "Mô hình chưa được triển khai"
+    },
+    "choise_dataset": {
+        "en": "No datasets are selected",
+        "vi": "Không có bộ dữ liệu nào được chọn"
+    },
+    "time_format": {
+        "en": "Time format does not match",
+        "vi": "Định dạng thời gian không phù hợp"
+    },
+    "model_notfound": {
+        "en" : "This model could not be found",
+        "vi": "Không tìm thấy mô hình"
+    },
+    "password_wrong": {
+        "en": "Password information is incorrect",
+        "vi": "Thông tin mật khẩu không đúng"
+    },
+    "account_exists": {
+        "en": "This account name already exists",
+        "vi": "Tài khoản này đã tồn tại"
+    },
+    "account_not_exists": {
+        "en": "Account does not exist",
+        "vi": "Tài khoản bị xóa không tồn tại"
+    },
+    "account_admin": {
+        "en": "Can't delete admin account",
+        "vi" :"Không xóa được tài khoản admin"
+    },
+    "work_strip": {
+        "en": "Unit name cannot be left blank",
+        "vi": "Tên đơn vị không được để trống"
+    },
+    "codework_strip": {
+        "en": "Unit code cannot be left blank",
+        "vi": "Mã đơn vị không được để trống"
+    },
+    "work_exists": {
+        "en": "This unit name already exists", 
+        "vi": "Tên đơn vị này đã tồn tại"
+    },
+    "codework_exists": {
+        "en": "This unit code already exists",
+        "vi": "Mã đơn vị này đã tồn tại"
+    },
+    "video_dataset_error": {
+        "en": "Add data by video failed",
+        "vi": "Thêm dữ liệu bằng video không thành công"
+    },
+    "camera_dataset_error": {
+        "en": "Add data by camera failed",
+        "vi": "Thêm dữ liệu bằng camera không thành công"
+    },
+    "jetson_not_exists": {
+        "en": "Jetson does not exist",
+        "vi": "Máy tính biên không tồn tại"
+    },
+    "jetson_name_exists": {
+        "en": "Jetson name already exist",
+        "vi": "Tên máy tính biên đã tồn tại"
+    },    
+    "upload_model_failed": {
+        "en": "Uploading model to jetson failed",
+        "vi": "Tải mô hình tới máy tính biên không thành công"
+    },
+
+    "camera_not_exists": {
+        "en": "Camera does not exist",
+        "vi": "Camera không tồn tại"
+    },
+    "domain_wrong": {
+        "en": "Domain is incorrect",
+        "vi": "Tên miền không đúng"
+    },
+    "serial_exists": {
+        "en": "Serial already exists",
+        "vi": "Serial đã tồn tại"
+    },
+    "api_manage_not_exists": {
+        "en": "API already exists",
+        "vi": "API đã tồn tại"
+    },
+    "invalid_input": {
+        "en": "Invalid input data",
+        "vi": "Dữ liệu đầu vào không hợp lệ"
+    },
+    "label_conflict": {
+        "en": "The new label conflicts with the old label",
+        "vi": "Nhãn mới bị trùng với nhãn cũ"
+    },
+    "download_model_failed": {
+        "en": "Download model failed",
+        "vi": "Tải xuống mô hình thất bại"
+    },
+    "Missing refresh token":{
+        "en": "Missing refresh token",
+        "vi": "Thiếu refresh token"
+    },
+    "Invalid refresh token":{
+        "en": "Invalid refresh token",
+        "vi": "Refresh token không hợp lệ"
+    },
+    "Refresh token revoked":{
+        "en": "Refresh token revoked",
+        "vi": "Refresh token đã bị thu hồi"
+    },
+    "update_camera_fail": {
+        "en": "Update camera information failed",
+        "vi": "Cập nhật thông tin camera không thành công"
+    },
+    "delete_video_db": {
+        "en": "File not found, delete video from database",
+        "vi": "Xoá video khỏi cơ sở dữ liệu nhưng không tìm thấy file"
+    }
+}
